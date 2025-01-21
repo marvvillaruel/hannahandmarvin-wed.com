c7fb22c7207f1783071c4afd4e720526164a26a5
Marvin Villaruel <archaanine@gmail.com>
2025-01-21 16:48:44 +0800

    edit01213

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ To set the countdown date see below section in `js/clock.js`
 
 ````
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2023-10-29 12:00", "Asia/Kolkata");
+  let targetDate = moment.tz("2023-10-29 12:00", "Asia/Manila");
 ````
 
 ## Sreenshot
--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2025-04-20 15:00", "Asia/Manila");
+  let targetDate = moment.tz("2025-04-20 15:30", "Asia/Manila");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
